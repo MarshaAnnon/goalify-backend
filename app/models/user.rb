030121f8024e6fd6116cbabbe16bd74f5b82fa0d
marsha <marshaannon@gmail.com>
2020-07-25 19:07:14 -0400

    added relationships, has secure password, & validations for email

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,11 @@
 class User < ApplicationRecord
+
+    has_many :topics
+    has_many :goals, through: :topics, dependent: :delete_all
+    
+    has_secure_password
+
+    validates_presence_of :email
+    validates_uniqueness_of :email
+    
 end
