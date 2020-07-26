0b948548a79ce6cab4bb277e4d6d4a46e4401317
marsha <marshaannon@gmail.com>
2020-07-25 20:04:39 -0400

    namespaced topics and goals

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,11 @@
 Rails.application.routes.draw do
   
-  resources :goals
-  resources :topics
+  
   namespace :api do
     namespace :v1 do
       resources :users
+      resources :topics
+      resources :goals
     end
   end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
