20f1785aebdc96873ad98359d998f3dc5fc06c48
marsha <marshaannon@gmail.com>
2020-07-26 15:17:45 -0400

    added sessions controller

---
+++ app/controllers/api/v1/sessions_controller.rb
@@ -0,0 +1,25 @@
+class Api::V1::SessionsController < ApplicationController
+
+    def create
+        @user = User.find_by(email: params[:session][:email])
+        if @user && @user.authenticate(params[:session][:password])
+            session[:user_id] = @user.id
+            render json: @user
+        else
+            render json: {
+                error: "Invalid Email or Password"
+            }
+        end
+    end
+
+    def get_current_user
+        if logged_in?
+            render json: current_user
+        else
+            render json: {
+                error: "Not Logged In"
+            }
+        end
+    end
+
+end
