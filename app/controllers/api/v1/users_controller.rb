5776a364846a0447ec1dec9558805683d1cfc61f
marsha <marshaannon@gmail.com>
2020-07-27 20:48:01 -0400

    added topics to user create action

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,32 +1,30 @@
 class Api::V1::UsersController < ApplicationController
   before_action :set_user, only: [:show, :update, :destroy]
 
-  # GET /users
-  def index
-    @users = User.all
-
-    users_json = UserSerializer.new(@users).serialized_json
-    # render json: @users
-  end
-
-  # GET /users/1
   def show
     user_json = UserSerializer.new(@user).serialized_json
     render json: user_json
   end
 
-  # POST /users
   def create
     @user = User.new(user_params)
-
+    
     if @user.save
-      render json: @user, status: :created, location: @user
+      topics = ["Personal Development", "Finance","Travel", "Lifestyle", "Career", "Friends & Family", "Giving Back/Legacy", "Hobbies & Passions", "Adventure"]
+      topics.each do | topic |
+        @user.topics.build(name: topic)
+      end
+      @user.save
+      session[:user_id] = @user.id
+      render json: UserSerializer.new(@user), status: :created
     else
-      render json: @user.errors, status: :unprocessable_entity
+      resp = {
+        error: @user.errors.full_messages.to_sentence
+      }
+      render json: resp, status: :unprocessable_entity
     end
   end
 
-  # PATCH/PUT /users/1
   def update
     if @user.update(user_params)
       render json: @user
@@ -35,19 +33,16 @@ class Api::V1::UsersController < ApplicationController
     end
   end
 
-  # DELETE /users/1
   def destroy
     @user.destroy
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
     def set_user
       @user = User.find_by_id(params[:id])
     end
 
-    # Only allow a trusted parameter "white list" through.
     def user_params
-      params.require(:user).permit(:username, :email, :password_digest)
+      params.require(:user).permit(:username, :email, :password)
     end
 end
