2c34b91fcbf14960895480749ff90131000c0c5e
marsha <marshaannon@gmail.com>
2020-07-28 20:46:12 -0400

    changed create method, added destroy

--- app/controllers/api/v1/topics_controller.rb
+++ app/controllers/api/v1/topics_controller.rb
@@ -1,7 +1,6 @@
 class Api::V1::TopicsController < ApplicationController
   before_action :set_topic, only: [:show, :update, :destroy]
 
-  # GET /topics
   def index
     if logged_in?
       @topics = current_user.topics
@@ -13,17 +12,15 @@ class Api::V1::TopicsController < ApplicationController
     end
   end
 
-  # GET /topics/1
   def show
     render json: @topic
   end
 
-  # POST /topics
   def create
-    @topic = Topic.new(topic_params)
+    @topic = current_user.topics.build(topic_params)
 
     if @topic.save
-      render json: @topic, status: :created
+      render json: TopicSerializer.new(@topic), status: :created
     else
       error_resp = {
         error: @topic.errors.full_messages.to_sentence
@@ -32,27 +29,33 @@ class Api::V1::TopicsController < ApplicationController
     end
   end
 
-  # PATCH/PUT /topics/1
   def update
     if @topic.update(topic_params)
-      render json: @topic
+      render json: TopicSerializer.new(@topic), status: :ok
     else
-      render json: @topic.errors, status: :unprocessable_entity
+      error_resp = {
+        error: @topic.errors.full_messages.to_sentence
+      }
+      render json: error_resp, status: :unprocessable_entity
     end
   end
 
-  # DELETE /topics/1
   def destroy
-    @topic.destroy
+    if @topic.destroy
+      render json: { data: "Topic successfully destroyed" }, status: :ok
+    else
+      error_resp = {
+        error: "Topic not found, so could not be destoyed"
+      }
+      render json: error_resp, status: :unprocessable_entity
+    end
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
     def set_topic
       @topic = Topic.find_by_id(params[:id])
     end
 
-    # Only allow a trusted parameter "white list" through.
     def topic_params
       params.require(:topic).permit(:name, :user_id)
     end
