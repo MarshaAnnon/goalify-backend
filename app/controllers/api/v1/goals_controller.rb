866c5afeb162f7ff8bb15e27fc190d4f992a9ad6
marsha <marshaannon@gmail.com>
2020-08-08 23:38:14 -0400

    remove commented out code, remove code bloat

--- app/controllers/api/v1/goals_controller.rb
+++ app/controllers/api/v1/goals_controller.rb
@@ -1,21 +1,6 @@
 class Api::V1::GoalsController < ApplicationController
   before_action :set_goal, only: [:show, :update, :destroy]
 
-  # def index
-  #   if logged_in?
-  #     @goals = current_user.goals
-  #   render json: GoalSerializer.new(@goals)
-  # else 
-  #   render json: {
-  #     error: "Must be logged in to see topics"
-  #   }
-  # end
-  # end
-
-  # def show
-  #   render json: @goal
-  # end
-
   def create
     @goal = Goal.new(goal_params)
     if @goal.save
@@ -27,14 +12,6 @@ class Api::V1::GoalsController < ApplicationController
       render json: error_resp, status: :unprocessable_entity
     end
   end
-
-  # def update
-  #   if @goal.update(goal_params)
-  #     render json: @goal
-  #   else
-  #     render json: @goal.errors, status: :unprocessable_entity
-  #   end
-  # end
 
   def destroy
     if @goal.destroy
@@ -48,12 +25,11 @@ class Api::V1::GoalsController < ApplicationController
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
+
     def set_goal
       @goal = Goal.find_by_id(params[:id])
     end
 
-    # Only allow a trusted parameter "white list" through.
     def goal_params
       params.require(:goal).permit(:goal_name, :goal_description, :goal_timeline, :topic_id)
     end
