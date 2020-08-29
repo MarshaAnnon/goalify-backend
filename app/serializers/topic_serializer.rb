e516d18367b42d51fc23fa83a293a1fe125935b5
marsha <marshaannon@gmail.com>
2020-08-28 22:03:23 -0400

    remove commented out code

--- app/serializers/topic_serializer.rb
+++ app/serializers/topic_serializer.rb
@@ -3,16 +3,5 @@ class TopicSerializer
   attributes :name, :goals
 
   belongs_to :user
-  
-  # attribute :goals do |topic|
-  #   topic.goals.map do |top|
-  #     {
-  #       goal_name: top.goal_name,
-  #       goal_description: top.goal_description,
-  #       goal_timeline: top.goal_timeline
-  #     }
-  #   end
-  # end
-
 
 end
