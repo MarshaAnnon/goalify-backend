e1d72af0fd63fd3a81b5105df3523e147671e10e
marsha <marshaannon@gmail.com>
2020-08-28 22:01:15 -0400

    clean up white space

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,6 @@ Rails.application.routes.draw do
   post "api/v1/signup", to: "api/v1/users#create"
   delete "api/v1/logout", to: "api/v1/sessions#destroy"
   get "api/v1/get_current_user", to: "api/v1/sessions#get_current_user"
-
   
   namespace :api do
     namespace :v1 do
@@ -13,5 +12,4 @@ Rails.application.routes.draw do
       resources :goals
     end
   end
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
